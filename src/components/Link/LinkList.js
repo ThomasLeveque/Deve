e6f032986541f87c1b0c9f2f7e88fc8b4aafa68d
Thomas Leveque <thomasl5895@gmail.com>
2019-06-25 14:28:49 +0200

    Pagination Done

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import FirebaseContext from '../../firebase/context';
 import LinkItem from './LinkItem';
+import { LINKS_PER_PAGE } from '../../utils/index';
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
+  const [cursor, setCursor] = React.useState(null);
   const isTopPage = props.location.pathname.includes('top');
+  const isNewPage = props.location.pathname.includes('new');
+  const page = Number(props.match.params.page);
 
   React.useEffect(() => {
     const unsubscribe = getLinks();
     return () => unsubscribe();
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   const getLinks = () => {
+    const hasCursor = Boolean(cursor);
     if (isTopPage) {
       return firebase.db
         .collection('links')
         .orderBy('voteCount', 'desc')
         .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection('links')
+        .orderBy('created', 'desc')
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection('links')
+        .orderBy('created', 'desc')
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
     }
-
-    return firebase.db
-      .collection('links')
-      .orderBy('created', 'desc')
-      .onSnapshot(handleSnapshot);
   };
 
   const handleSnapshot = snapshot => {
@@ -33,8 +46,24 @@ function LinkList(props) {
         ...doc.data()
       };
     });
+    const lastLink = links[links.length - 1];
     setLinks(links);
+    setCursor(lastLink);
   };
+
+  const visitePreviousPage = () => {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  };
+
+  const visiteNextPage = () => {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  };
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 1;
 
   return (
     <div>
@@ -43,9 +72,19 @@ function LinkList(props) {
           key={link.id}
           showCount={true}
           link={link}
-          index={index + 1}
+          index={index + pageIndex}
         />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitePreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visiteNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
