42ac03c94661e1cdb5116274370edd52ba88ac03
Thomas Leveque <thomasl5895@gmail.com>
2019-06-27 17:35:01 +0200

    Switch all file to typescript

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACKzmyQVwDpckw9qd0BRdXR8u0VN_pfJY",
+  authDomain: "hooks-news-9c4b2.firebaseapp.com",
+  databaseURL: "https://hooks-news-9c4b2.firebaseio.com",
+  projectId: "hooks-news-9c4b2",
+  storageBucket: "",
+  messagingSenderId: "1064362918401",
+  appId: "1:1064362918401:web:08d63baba9e2f8e2"
+};
+
+export default firebaseConfig;
