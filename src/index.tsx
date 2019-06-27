2a838d12effff682b7e2f04646c9ad5a977c41e6
Thomas Leveque <thomasl5895@gmail.com>
2019-06-27 17:48:35 +0200

    one more file to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
