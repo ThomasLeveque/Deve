39b759b9dfd643bdd08108c50ea220d7016498bd
Thomas Leveque <thomasl5895@gmail.com>
2019-12-20 18:45:57 +0100

    update vars

--- src/app/components/Bar/progress-bar.tsx
+++ src/app/components/Bar/progress-bar.tsx
@@ -26,12 +26,12 @@ const ProgressBar: React.FC<IProps> = ({ isAnimating }) => {
     >
       <div
         style={{
-          background: '#0973ba',
+          background: '#D86334',
           height: 2,
           left: 0,
           marginLeft: `${(-1 + progress) * 100}%`,
           position: 'fixed',
-          top: 60,
+          top: 64,
           transition: `margin-left ${animationDuration}ms linear`,
           width: '100%',
           zIndex: 1031
