27ea7b87bc4ab745303e97f5458aded26549642a
Thomas Leveque <thomasl5895@gmail.com>
2020-01-31 12:58:36 +0100

    update cloud function from ts to js

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+const algoliasearch = require('algoliasearch');
+
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('dev_links');
+
+exports.addToIndex = functions.firestore.document('links/{linkId}').onCreate(snapshot => {
+  const data = snapshot.data();
+  const objectID = snapshot.id;
+
+  return index.addObject({ ...data, objectID });
+});
+
+exports.updateIndex = functions.firestore.document('links/{linkId}').onUpdate(change => {
+  const newData = change.after.data();
+  const objectID = change.after.id;
+  return index.saveObject({ ...newData, objectID });
+});
+
+exports.deleteFromIndex = functions.firestore.document('links/{linkId}').onDelete(snapshot => index.deleteObject(snapshot.id));
