ccf05e4466e75a92cfeef344226ff24b1c16c32f
Thomas Leveque <thomasl5895@gmail.com>
2020-02-20 17:06:50 +0100

    add algolia logo to filter-bar

--- src/app/components/filter-bar/filter-bar.component.tsx
+++ src/app/components/filter-bar/filter-bar.component.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Stats } from 'react-instantsearch-dom';
 
+import { ReactComponent as AlgoliaLogo } from '../../../assets/images/algolia-logo.svg';
 import SearchInput from '../search-input/search-input.component';
 import RefinementCategories from '../refinement-categories/refinement-categories.component';
 import ClearRefinementCategories from '../clear-refinement-categories/clear-refinement-categories.component';
@@ -7,10 +9,12 @@ import ClearRefinementCategories from '../clear-refinement-categories/clear-refi
 import './filter-bar.styles.less';
 
 const FilterBar: React.FC = () => {
-
-
   return (
     <div className="filter-bar">
+      <div className="result-logo">
+        <Stats />
+        <AlgoliaLogo />
+      </div>
       <h4>Filter by search :</h4>
       <SearchInput />
       <h4>Filter by categories :</h4>
