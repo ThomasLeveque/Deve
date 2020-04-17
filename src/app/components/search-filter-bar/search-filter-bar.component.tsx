e7838f5175dfeee15d7e01003e76d60747c2e349
Thomas Leveque <thomasl5895@gmail.com>
2020-04-17 14:52:50 +0200

    update facet limit

--- src/app/components/search-filter-bar/search-filter-bar.component.tsx
+++ src/app/components/search-filter-bar/search-filter-bar.component.tsx
@@ -10,7 +10,7 @@ const SearchFilterBar: React.FC = () => {
     <div className="search-filter-bar">
       <h4>Search by categories :</h4>
       <ClearRefinementCategories />
-      <RefinementCategories attribute="categories" />
+      <RefinementCategories attribute="categories" limit={99} />
     </div>
   );
 };
