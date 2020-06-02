417224a12a6a5e31e438724f6576c773b574666b
ThomasLeveque <thomasl5895@gmail.com>
2020-06-02 10:33:03 +0200

    fixes return type and new line at the end of a file

--- src/app/hooks/debounce.hook.ts
+++ src/app/hooks/debounce.hook.ts
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 
 // Our hook
-const useDebounce = (value: string, delay: number) => {
+const useDebounce = (value: string, delay: number): string => {
   // State and setters for debounced value
-  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [debouncedValue, setDebouncedValue] = useState<string>(value);
 
   useEffect(() => {
     const handler = setTimeout(() => {
